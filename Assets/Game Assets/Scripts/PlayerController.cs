133eaf99f7824df80188244bf71c1257bdc57c27
ayushgaikwad26 <ayushgaikwad48@gmail.com>
2025-04-12 18:15:08 +0530

    s

--- Assets/Game Assets/Scripts/PlayerController.cs
+++ Assets/Game Assets/Scripts/PlayerController.cs
@@ -5,24 +5,32 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
+    public BoxCollider2D boxCollider; // Reference to your existing BoxCollider2D
+
+    private Vector2 originalColliderSize;
+    private float crouchHeight = 0.8f; // You can tweak this value as needed
 
     private void Awake()
     {
         Debug.Log("Player controller awake");
     }
 
-    // private void OnCollisionEnter2D(Collision2D collision)
-    // {
-    //     Debug.Log("Collision: " + collision.gameObject.name);
-    // }
+    private void Start()
+    {
+        // Store original size of collider for restoring later
+        originalColliderSize = boxCollider.size;
+    }
 
     private void Update()
     {
         float speed = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        // Set walking/running animation
         animator.SetFloat("Speed", Mathf.Abs(speed));
 
+        // Flip player based on direction
         Vector3 scale = transform.localScale;
-
         if (speed < 0)
         {
             scale.x = -1 * Mathf.Abs(scale.x);
@@ -31,7 +39,23 @@ public class PlayerController : MonoBehaviour
         {
             scale.x = Mathf.Abs(scale.x);
         }
-
         transform.localScale = scale;
+
+        // Crouch logic
+        bool Crouch = Input.GetKey(KeyCode.LeftControl);
+        animator.SetBool("Crouch", Crouch);
+
+        if (Crouch)
+        {
+            boxCollider.size = new Vector2(boxCollider.size.x, crouchHeight);
+        }
+        else
+        {
+            boxCollider.size = originalColliderSize;
+        }
+
+        // Jump logic
+        bool Jump = vertical > 0;
+        animator.SetBool("Jump", Jump);
     }
 }
